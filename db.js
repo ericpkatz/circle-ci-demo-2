9a45327f155163fdbc3ae9b87b4fb26fdc75699a
Eric P. Katz <ericpkatz@gmail.com>
2019-05-21 16:32:27 -0400

    add db

---
+++ db.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize');
+const conn = new Sequelize(process.env.DATABASE_URL);
+
+const User = conn.define('user', {
+  name: Sequelize.STRING
+});
+
+const syncAndSeed = ()=> {
+  return conn.sync({ force: true })
+    .then( ()=> User.create({ name: 'moe'}));
+};
+
+module.exports = {
+  syncAndSeed,
+  User
+};
--- test.spec.js
+++ test.spec.js
@@ -1,6 +1,12 @@
+const db = require('./db');
 const expect = require('chai').expect;
 describe('my app', ()=> {
+  beforeEach(()=> db.syncAndSeed());
   it('can test', ()=> {
     expect(true).to.equal(true);
+  });
+  it('moe is there', ()=> {
+    return db.User.findOne({ name: 'moe'})
+      .then( moe => expect(moe.name).to.equal('moe'));
   });
 });
